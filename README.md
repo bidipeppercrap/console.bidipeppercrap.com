78964592520fbac9353899ddfd86f49325c5fc2d
bidipeppercrap <bidipeppercrap@outlook.com>
2020-08-01 21:40:13 +0700

    Prettify readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Console
+# Console - bidipeppercrap.com
+
+Console on the cloud.
+
+## Technology Used
+[<img align="left" width="26px" src="https://svelte.dev/favicon.png" alt="svelte">][technologies]
+[<img align="left" width="26px" src="https://www.netlify.com/img/press/logos/logomark.png" alt="netlify">][technologies]
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
 	import '../node_modules/plane.css';
 	import { onMount } from 'svelte';
-	import { Router, Link, Route, navigate } from 'svelte-routing';
+	import { Router, link, Route, navigate } from 'svelte-routing';
 	import { accessToken } from './stores';
 
 	import Login from './routes/Login.svelte';
+	import Home from './routes/Home.svelte';
 
 	export let url = '';
+
+	let toggleMenu = false;
 
 	onMount(() => {
 		if(!$accessToken) {
@@ -14,9 +17,29 @@
 		}
 	});
 
+	function handleToggle() {
+		toggleMenu = !toggleMenu;
+	}
+
 </script>
 
 <Router {url}>
 	<Route path="login" component={Login}/>
-	<Route path="/"/>
+	{#if location.pathname != 'login'}
+		<nav class="plane navbar">
+			<a class="navbar-logo img btn plane" href="/" use:link><img src="logo_long.svg" alt="logo" height="24px"></a>
+			<btn class="navbar-hamburger btn plane" on:click={handleToggle}>{toggleMenu ? 'x' : 'menu'}</btn>
+			<a class="navbar-item btn plane" href="/posts" use:link>posts</a>
+			<a class="navbar-item btn plane" href="/projects" use:link>projects</a>
+			<a class="navbar-item btn plane" href="/favorites" use:link>favorites</a>
+			<a class="navbar-item btn plane" href="/" use:link></a>
+			<a class="navbar-item btn plane" href="/" use:link></a>
+		</nav>
+		<div class="wrapper">
+			<Route path="favorites"/>
+			<Route path="projects"/>
+			<Route path="posts"/>
+			<Route path="/" component={Home}/>
+		</div>
+	{/if}
 </Router>
