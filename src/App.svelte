5d1b7066a62a8f5127520ba18a615b31cb873f88
bidipeppercrap <bidipeppercrap@outlook.com>
2020-08-03 20:36:29 +0700

    Navbar toggle fix

--- src/App.svelte
+++ src/App.svelte
@@ -29,8 +29,8 @@
 	<div class="navbar-wrapper">
 		<nav class="plane navbar">
 			<a class="navbar-logo img btn" href="/" use:link><img src="logo_long.svg" alt="logo" height="24px"></a>
-			<btn class={ (toggleMenu ? 'alert ' : '') + "navbar-hamburger btn"} on:click={handleToggle}>{toggleMenu ? 'x' : 'menu'}</btn>
-			<div class="navbar-items" style={toggleMenu ? 'display: grid;' : 'display: none;'}>
+			<btn class={(toggleMenu ? "alert " : "") + "navbar-hamburger btn"} on:click={handleToggle}>{toggleMenu ? 'x' : 'menu'}</btn>
+			<div class={(toggleMenu ? "navbar-items-toggled " : "") + "navbar-items"}>
 				<a class="navbar-item btn" href="/posts" use:link>posts</a>
 				<a class="navbar-item btn" href="/projects" use:link>projects</a>
 				<a class="navbar-item btn" href="/favorites" use:link>favorites</a>
