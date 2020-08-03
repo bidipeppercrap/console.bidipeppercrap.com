fa4d951610db3bd759c3e33d83dbb2970216478c
bidipeppercrap <bidipeppercrap@outlook.com>
2020-08-03 22:22:03 +0700

    Final Navbar complete

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,8 @@
 	import Login from './routes/Login.svelte';
 	import Home from './routes/Home.svelte';
 
+	import NavLink from './NavLink.svelte'
+
 	export let url = '';
 
 	let toggleMenu = false;
@@ -28,14 +30,14 @@
 	{#if location.pathname != 'login'}
 	<div class="navbar-wrapper">
 		<nav class="plane navbar">
-			<a class="navbar-logo img btn" href="/" use:link><img src="logo_long.svg" alt="logo" height="24px"></a>
+			<NavLink className="navbar-logo img btn" to="/"><img src="logo_long.svg" alt="logo" height="24px"></NavLink>
 			<btn class={(toggleMenu ? "alert " : "") + "navbar-hamburger btn"} on:click={handleToggle}>{toggleMenu ? 'x' : 'menu'}</btn>
 			<div class={(toggleMenu ? "navbar-items-toggled " : "") + "navbar-items"}>
-				<a class="navbar-item btn" href="/posts" use:link>posts</a>
-				<a class="navbar-item btn" href="/projects" use:link>projects</a>
-				<a class="navbar-item btn" href="/favorites" use:link>favorites</a>
-				<a class="navbar-item btn" href="/" use:link>wishlist</a>
-				<a class="navbar-item btn" href="/" use:link>contacts</a>
+				<NavLink className="navbar-item btn" to="/posts">posts</NavLink>
+				<NavLink className="navbar-item btn" to="/projects">projects</NavLink>
+				<NavLink className="navbar-item btn" to="/favorites">favorites</NavLink>
+				<NavLink className="navbar-item btn" to="/wishlist">wishlist</NavLink>
+				<NavLink className="navbar-item btn" to="/contacts">contacts</NavLink>
 			</div>
 		</nav>
 	</div>
---
+++ src/NavLink.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { Link } from "svelte-routing";
+
+    export let to = "";
+    export let className = "";
+
+    function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+        const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
+
+        if (isActive) {
+            return { class: className + " active" };
+        }
+
+        return {
+            class: className
+        };
+    }
+
+</script>
+
+<Link {className} {to} {getProps}>
+    <slot />
+</Link>
