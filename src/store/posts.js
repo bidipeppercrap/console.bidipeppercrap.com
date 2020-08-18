8212dcf86a3fe90f9a00247e03d002ddc65bc8e0
bidipeppercrap <bidipeppercrap@outlook.com>
2020-08-18 22:36:26 +0700

    update package.json

--- src/store/posts.js
+++ src/store/posts.js
@@ -1,1 +1,18 @@
-import { writeable } from "svelte/store";
+import { writeable, readable } from "svelte/store";
+import axios from "axios";
+
+import { accessToken } from "./auth";
+
+export const posts = writeable([]);
+
+export const fetchPost = () => {
+    const { data } = axios({
+        method: "GET",
+        url: "posts",
+        headers: {
+            "Authorization": `Bearer ${accessToken}`
+        }
+    });
+    
+    posts.update(data);
+}
